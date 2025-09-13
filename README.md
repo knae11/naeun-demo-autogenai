33126efe1c6119e395e9f214399a9069e12682fc
naeunkim <knae11@naver.com>
2025-09-13 22:42:12 +0100

    init hello world

---
+++ README.md
@@ -0,0 +1,8 @@
+# Converting TS repo to Java
+- backend server
+
+## Todo
+- [] GET `/` returns "hello world"
+- [] POST `/summerise` returns "result"
+- [] GET `/usage` returns list
+- [] test codes
---
+++ src/main/java/com/example/naeun_demo/NaeunDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.naeun_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaeunDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaeunDemoApplication.class, args);
+	}
+
+}
