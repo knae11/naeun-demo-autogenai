bd0cf05b7b6b57fa88ccb0a7cf4fda692d2e6f5e
naeunkim <knae11@naver.com>
2025-09-15 17:15:51 +0100

    fix: mapping run server

--- src/main/java/com/example/naeundemo/activity/dto/ActivityRequestDto.java
+++ src/main/java/com/example/naeundemo/activity/dto/ActivityRequestDto.java
@@ -5,11 +5,16 @@ import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.jetbrains.annotations.NotNull;
 
+@Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ActivityRequestDto {
+    @NotNull
     private String model;
+    @NotNull
     private LocalDate since;
 }
